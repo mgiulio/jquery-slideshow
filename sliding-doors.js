b080619a309616c0be99f1bf321b8029154e876c
mgiulio <giulio.mainardi@gmail.com>
2011-08-30 08:28:52 +0200

    Port 'sliding doors' to the new framework

--- sliding-doors.js
+++ sliding-doors.js
@@ -1,84 +1,90 @@
-transitions.slidingDoors = function (axis) {
-	var
-		front = buff[visibleBuff],
-		back = buff[1-visibleBuff],
-		duration = 3000,
-		zIndex = front.css('zIndex'),
-		doorCss = {
-			position: 'absolute',
-			backgroundImage: 'url(' + front.attr('src') + ')',
-			zIndex: zIndex
-		},
-		doors = [
-			{
-				node: $('<div class="left-door"></div>'), // FIXME
-				css: $.extend({}, doorCss, axis === 'horizontal' ?
-					{
-						width: frameWidth / 2 + 'px',
-						height: frameHeight + 'px',
-						left: 0,
-						top: 0,
-						backgroundPosition: 'left top'
-					}
-					:
-					{
-						width: frameWidth + 'px',
-						height:  frameHeight / 2 + 'px',
-						left: 0,
-						top: 0,
-						backgroundPosition: 'left top'
-					}),
-				animCss: axis === 'horizontal' ? 
-					{left: -frameWidth/2 + 'px'} :
-					{top: -frameHeight + 'px'}
+transitions['sliding doors'] = $.extend(Object.create(transition), {
+	axis: 'horizontal',
+	cfg: function(cfg) {
+		if ('axis' in cfg)
+			this.axis = cfg.axis;
+	},
+	play: function() {
+		var 
+			self = this,
+			zIndex = this.front.css('zIndex'),
+			doorCss = {
+				position: 'absolute',
+				backgroundImage: 'url(' + this.front.attr('src') + ')',
+				zIndex: zIndex
 			},
-			{
-				node: $('<div class="right-door"></div>'), //FIXME
-				css: $.extend({}, doorCss, axis === 'horizontal' ?
-					{
-						width: frameWidth / 2 + 'px',
-						height: frameHeight + 'px',
-						right: 0,
-						top: 0,
-						backgroundPosition: 'right top'
-					}
-					:
-					{
-						width: frameWidth + 'px',
-						height:  frameHeight / 2 + 'px',
-						left: 0,
-						bottom: 0,
-						backgroundPosition: 'left bottom'
-					}),
-				animCss: axis === 'horizontal' ? 
-					{right: -frameWidth/2 + 'px'} :
-					{bottom: -frameHeight + 'px'}
-			}
-		],
-		d0 = doors[0],
-		d1 = doors[1]
-	;
+			doors = [
+				{
+					node: $('<div class="left-door"></div>'), // FIXME
+					css: $.extend({}, doorCss, this.axis === 'horizontal' ?
+						{
+							width: this.frameWidth / 2 + 'px',
+							height: this.frameHeight + 'px',
+							left: 0,
+							top: 0,
+							backgroundPosition: 'left top'
+						}
+						:
+						{
+							width: this.frameWidth + 'px',
+							height:  this.frameHeight / 2 + 'px',
+							left: 0,
+							top: 0,
+							backgroundPosition: 'left top'
+						}),
+					animCss: this.axis === 'horizontal' ? 
+						{left: -this.frameWidth/2 + 'px'} :
+						{top: -this.frameHeight + 'px'}
+				},
+				{
+					node: $('<div class="right-door"></div>'), //FIXME
+					css: $.extend({}, doorCss, this.axis === 'horizontal' ?
+						{
+							width: this.frameWidth / 2 + 'px',
+							height: this.frameHeight + 'px',
+							right: 0,
+							top: 0,
+							backgroundPosition: 'right top'
+						}
+						:
+						{
+							width: this.frameWidth + 'px',
+							height:  this.frameHeight / 2 + 'px',
+							left: 0,
+							bottom: 0,
+							backgroundPosition: 'left bottom'
+						}),
+					animCss: this.axis === 'horizontal' ? 
+						{right: -this.frameWidth/2 + 'px'} :
+						{bottom: -this.frameHeight + 'px'}
+				}
+			],
+			d0 = doors[0],
+			d1 = doors[1]
+		;
+		
+		slideshow.append(
+			d0.node.css(d0.css),
+			d1.node.css(d1.css)
+		);
 	
-	slideshow.append(
-		d0.node.css(d0.css),
-		d1.node.css(d1.css)
-	);
-	front.hide();
+		this.front.hide();
 
-	$.when(
-		d0.node.animate(
-			d0.animCss,
-			duration,
-			'easeOutQuad'
-		),
-		d1.node.animate(
-			d1.animCss,
-			duration,
-			'easeOutQuad'
-		)
-	).then(function() {
-		d0.node.remove();
-		d1.node.remove();
-		transitions._afterTransition();
-	});
-};
+		$.when(
+			d0.node.animate(
+				d0.animCss,
+				this.duration,
+				'easeOutQuad'
+			),
+			d1.node.animate(
+				d1.animCss,
+				this.duration,
+				'easeOutQuad'
+			)
+		).then(function() {
+			d0.node.remove();
+			d1.node.remove();
+			self.done();
+		});
+	}
+});
