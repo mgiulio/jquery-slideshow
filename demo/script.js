003e25f57ed0f387ba387975a19bbb1da8bec6b4
mgiulio <giulio.mainardi@gmail.com>
2011-09-01 14:28:53 +0200

    Demo: remove old code

--- demo/script.js
+++ demo/script.js
@@ -18,30 +18,3 @@ $('#frame #transitions').delegate('input', 'click', function(e) {
 	ss.slideshow('option', 'transition', e.target.value.toLowerCase());
 });
 
-/* 
-
-$('#sliding-door').delegate('button', 'click', function(e) {
-	ss.slideshow('option', 'transition', 'sliding door ' + $(e.target).text());
-	ss.slideshow('next');
-});
-
-$('#sliding-doors').delegate('button', 'click', function(e) {
-	ss.slideshow('option', 'transition', 'sliding doors ' + $(e.target).text());
-	ss.slideshow('next');
-});
-
-$('#slide').delegate('button', 'click', function(e) {
-	ss.slideshow('option', 'transition', 'slide ' + $(e.target).text());
-	ss.slideshow('next');
-});
-
-$('#none').click(function() {
-	ss.slideshow('option', 'transition', 'none');
-	ss.slideshow('next');
-	return false;
-});
-
-$('#cross-fade').click(function() {
-	ss.slideshow('option', 'transition', 'cross fade');
-	ss.slideshow('next');
-}); */
